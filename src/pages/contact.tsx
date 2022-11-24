ccd6c82b7eae3d810cc782ddeea064cf7bc30e7b
mu0363 <dde036@gmail.com>
2022-11-24 11:42:54 +0900

    add contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -12,6 +12,11 @@ const schema = z.object({
     .string()
     .email({ message: "Invalid email" })
     .min(1, { message: "Required" }),
+  person: z.union([
+    z.literal("患者本人"),
+    z.literal("家族"),
+    z.literal("その他"),
+  ]),
 });
 
 type Inputs = z.infer<typeof schema>;
@@ -23,6 +28,12 @@ const Contact = () => {
     formState: { errors },
   } = useForm<Inputs>({ resolver: zodResolver(schema) });
   const onSubmit: SubmitHandler<Inputs> = (data) => console.info(data);
+
+  const personTypes = [
+    { id: "患者本人", title: "患者本人" },
+    { id: "家族", title: "家族" },
+    { id: "その他", title: "その他" },
+  ];
 
   return (
     <>
@@ -69,29 +80,66 @@ const Contact = () => {
               </div>
             </div>
 
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                      clipRule="evenodd"
+            <fieldset className="mt-4">
+              <div className="flex items-center space-x-5">
+                {personTypes.map((personType) => (
+                  <div key={personType.id} className="flex items-center">
+                    <input
+                      id={personType.id}
+                      type="radio"
+                      value={personType.title}
+                      defaultChecked={personType.id === "患者本人"}
+                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      {...register("person")}
                     />
-                  </svg>
-                </span>
-                送信
-              </button>
+                    <label
+                      htmlFor={personType.id}
+                      className="ml-3 block text-sm font-medium text-gray-700"
+                    >
+                      {personType.title}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              {errors.person && <span>{errors.person.message}</span>}
+            </fieldset>
+
+            <div className="sm:col-span-6">
+              <label htmlFor="content">
+                お問い合わせ内容
+                <div className="mt-1">
+                  <textarea
+                    id="content"
+                    name="content"
+                    rows={3}
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    defaultValue=""
+                  />
+                </div>
+              </label>
             </div>
+
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              送信
+            </button>
           </form>
         </section>
       </Layout>
