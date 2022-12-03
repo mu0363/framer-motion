890e50e2abdd759f57dc0b5fa5e0c2abc203df72
mu0363 <dde036@gmail.com>
2022-12-03 15:22:38 +0900

    new Dateに変更してみた

--- src/components/pages/article/ArticleCard/ArticleCard.tsx
+++ src/components/pages/article/ArticleCard/ArticleCard.tsx
@@ -1,4 +1,4 @@
-import { format, parseISO } from "date-fns";
+import { format } from "date-fns";
 import Link from "next/link";
 import type { FC } from "react";
 import type { ArticleType } from "src/types";
@@ -6,7 +6,9 @@ import type { ArticleType } from "src/types";
 type Props = Omit<ArticleType, "author" | "body" | "meta">;
 
 /** @package */
-export const ArticleCard: FC<Props> = ({ title, _id, _sys }) => {
+export const ArticleCard: FC<Props> = ({ title, _id }) => {
+  console.log(new Date());
+
   return (
     <div className="transition-colors hover:bg-green-50">
       <Link href={`/article/${_id}`}>
@@ -14,7 +16,7 @@ export const ArticleCard: FC<Props> = ({ title, _id, _sys }) => {
         <div className="flex items-center justify-between transition-transform hover:translate-x-2">
           <div className="flex items-center space-x-4 overflow-hidden p-3 lg:space-x-16">
             <span className="text-xs text-gray-400 xl:text-sm">
-              {format(parseISO(_sys.createdAt), "yyyy.MM.dd")}
+              {format(new Date(), "yyyy.MM.dd")}
             </span>
             <span className="truncate text-xs lg:text-base">{title}</span>
           </div>
