68c9746041a1ff469f673d2e4f844ec9df8654ca
mu0363 <dde036@gmail.com>
2022-12-15 12:46:13 +0900

    OGPを再設定

--- src/pages/article/content/[contentId].tsx
+++ src/pages/article/content/[contentId].tsx
@@ -23,9 +23,19 @@ const ArticleDetail: NextPage<ArticleType> = ({
   return (
     <>
       <Head>
-        <title>{meta.ogImage.title}</title>
-        <meta name="description" content={meta.ogImage.description} />
+        <title>{meta.title}</title>
+        <meta name="title" content={meta.title} />
+        <meta
+          name="og:url"
+          content={`${process.env.NEXT_PUBLIC_DOMAIN}${path}`}
+        />
+        <meta name="og:type" content="article" />
+        <meta name="og:title" content={meta.title} />
         <meta property="og:image" content={meta.ogImage.src} />
+        <meta name="og:description" content={meta.description} />
+        <meta name="og:description" content="JAAC" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@JAAC" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
